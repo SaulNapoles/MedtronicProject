67e8c00e12017f0b21542596cde187bb02fd6e2c
Saul Rodríguez Nápoles <saul_antonio_16@hotmail.com>
2017-11-04 10:49:48 -0700

    Delete all for up one more timec into a folder

--- MedtronicWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MedtronicWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- MedtronicWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MedtronicWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
