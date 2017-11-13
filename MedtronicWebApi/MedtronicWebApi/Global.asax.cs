4b0db12d87192a94c6ba9e0f36ac92679919f657
Daniel Torres <jesusdanieltc@gmail.com>
2017-11-13 13:44:36 -0700

    Database records
    Records of the database and file configuration Global.asx

--- MedtronicWebApi/MedtronicWebApi/Global.asax.cs
+++ MedtronicWebApi/MedtronicWebApi/Global.asax.cs
@@ -18,6 +18,11 @@ namespace MedtronicWebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+            .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
